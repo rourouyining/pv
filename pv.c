8aaac7dc3e9471e8eaec1cbcbe50d92d79db2b10
rourouyining <13264563694@163.com>
2018-09-04 10:41:52 +0800

    sem pv
    Signed-off-by: rourouyining <13264563694@163.com>

--- pv.c
+++ pv.c
@@ -3,55 +3,64 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define P(x)  sem_wait(&x)
-#define V(x)  sem_post(&x)
-#define n 8
+#define P(x) sem_wait(&x)
+#define V(x) sem_post(&x)
+
+
 sem_t empty;
 sem_t full;
-
+pthread_mutex_t mutex;
 void *producer(void *arg)
 {
 	while(1)
 	{
 		P(empty);
-		sleep(3);
+		pthread_mutex_lock(&mutex);
+		sleep(1);
 		printf("producer\n");
+		pthread_mutex_unlock(&mutex);
+		usleep(100);
 		V(full);
 	}
+
 }
 
-void *buy(void *arg)
+void *consumer(void *arg)
 {
 	while(1)
 	{
 		P(full);
+		pthread_mutex_lock(&mutex);
 		sleep(1);
-		printf("buy\n");
+		printf("consumer\n");
+		pthread_mutex_unlock(&mutex);
+		usleep(100);
 		V(empty);
-		
 	}
+
 }
 
 int main(int argc, const char *argv[])
 {
 	pthread_t id[2];
-
-	int log = sem_init(&empty, 0, n);
+	int log;
+	log = sem_init(&empty, 0, 8);
 	if(log != 0)
 	{
-		printf("sem_init erroe");
+		printf("sem_init empty error!");
 		return 0;
 	}
 	log = sem_init(&full, 0, 0);
 	if(log != 0)
 	{
-		printf("sem_init erroe");
+		printf("sem_init full error!");
 		return 0;
 	}
-
+	pthread_mutex_init(&mutex, NULL);
+	
 	pthread_create(&id[0], NULL, producer, NULL);
-	pthread_create(&id[1], NULL, buy, NULL);
-
+	pthread_create(&id[1], NULL, consumer, NULL);
+	
 	pthread_join(id[0], NULL);
 	pthread_join(id[1], NULL);
 
